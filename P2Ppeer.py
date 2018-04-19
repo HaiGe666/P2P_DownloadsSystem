c8dc76e757b3fc67966f0f835cc19d588090d36e
Henry Liu <Henry.Liuh@foxmail.com>
2018-04-19 21:35:16 +0800

    Add files via upload

---
+++ P2Ppeer.py
@@ -0,0 +1,198 @@
+# encoding:utf-8
+
+import socket as sc
+import os
+import threading
+
+HOST = '127.0.0.1'
+PORT = 10010
+
+PEERADDRPORT = ('127.0.0.1', 10086)
+
+class P2Pclient():
+    def __init__(self, desAddrPort):
+        self.desAddrPort = desAddrPort
+        self.client_socket = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+        self.client_socket.connect(self.desAddrPort)
+
+    def register(self):
+        print("register to server, please wait...")
+        self.client_socket.send('register'.encode())
+        dataObj = self.client_socket.recv(1024)
+        if not dataObj:
+            return False
+        elif dataObj.decode() == 'give files you have':
+            peerShareList = os.listdir(os.path.join(os.path.abspath('.'), 'share'))
+            self.client_socket.send(repr(peerShareList).encode())
+            print("registration success")
+            
+            #给服务器我的被download地址端口
+            dataObj1 = self.client_socket.recv(1024)
+            if not dataObj:
+                return False
+            elif dataObj1.decode() == 'give your dowAddrPort':
+                self.client_socket.send(repr(PEERADDRPORT).encode())
+                print("give my address and port", PEERADDRPORT, "to server for other peers to download")
+
+            #亟待建立一个非阻塞服务器用于下载?
+
+            return True
+        else:
+            return False
+
+    def getl(self):
+        self.client_socket.send('get file list'.encode())
+        listObj = self.client_socket.recv(1024)
+        if not listObj:
+            return False
+        else:
+            fileList = eval(listObj.decode())   #空列表？
+            print(fileList)
+            return True
+
+    def dowf(self):
+        self.client_socket.send('download file'.encode())
+        dataObj = self.client_socket.recv(1024)
+        if not dataObj:
+            print('1 ', end = '')
+            return False
+        elif dataObj.decode() == 'filename':
+            filename = input("input filename:")
+            self.client_socket.send(filename.encode())
+            ifExistObj = self.client_socket.recv(1024)   #4.18 18:22
+            if not ifExistObj:
+                print('2 ', end = '')
+                return False
+            elif ifExistObj.decode() == 'exist and ready':
+                peerAddrPortListObj = self.client_socket.recv(1024) #接受含文件的peer list
+                if not peerAddrPortListObj:
+                    print('3 ', end = '')
+                    return False
+                else:
+                    peerAddrPortList = eval(peerAddrPortListObj.decode())    #空列表...怎么办,不会是空列表的，否则就是不存在
+                    
+                    #重点，分片从peer下载，最好实现多线程下载
+
+                    self.dowFromPeers(filename, peerAddrPortList)
+                    return True
+                    
+            elif ifExistObj.decode() == 'not exist':
+                print("Sorry, we don't have " + filename + ' to download...')
+                return True
+            else:
+                print('4 ', end ='')
+                return False
+        else:
+            return False
+
+    def logOut(self):
+        self.client_socket.send('quit'.encode())
+        dataObj = self.client_socket.recv(1024)
+        if not dataObj:
+            return False
+        elif dataObj.decode() == 'log out success':
+            print("log out success")
+            return True
+        else:
+            return False
+
+    def dowFromPeers(self, filename, peersList):
+        #连接有文件peer的下载socket，而不是和服务器连接的socket呵呵
+        #拓展：实现多线程下载
+        print("downloading", filename, "from", peersList)
+        #先顺序
+        for i in range(len(peersList)):
+            print("downloading", filename, i, '|', len(peersList), 'please wait...')
+            s = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+            s.connect(peersList[i]) #目标主机积极拒绝无法连接
+            s.send(repr([filename, i, len(peersList)]).encode())
+            dataObj = s.recv(1024)
+            partFileSize = int(dataObj.decode())
+            recievedSize = 0
+
+            with open(filename + str(i), 'wb') as f:
+                while recievedSize < partFileSize:
+                    remainSize = partFileSize - recievedSize
+                    rs = 1024 if remainSize > 1024 else remainSize
+                    recFile = s.recv(rs)
+                    f.write(recFile)
+                    recievedSize += rs
+            print("completely download", filename, i, '|', len(peersList))
+
+        print("completely downloaded all", filename)
+        
+        self.merf(filename, len(peersList))
+        
+    def merf(self, filename, fileNumber):
+        print("Merging files...")
+        with open(filename, 'wb') as wf:
+            for i in range(fileNumber):
+                with open(filename + str(i), 'rb') as rf:
+                    data = rf.read()
+                    wf.write(data)
+        print("completely merge files")
+
+class SendFileThread(threading.Thread):
+    def run(self):
+        sendFileSc = sc.socket(sc.AF_INET, sc.SOCK_STREAM)    #可用TCPserver，但先不用
+        sendFileSc.bind(PEERADDRPORT)
+        sendFileSc.listen(1)
+        while True:  #待完成，希望是服务器？
+            wantDowSc, wantDowAddrPort = sendFileSc.accept()    #完成一个peer的传输任务0以后accept是否可以继续接受其它peer
+            print('\n', "Peer", wantDowAddrPort, "is connecting...", end = '\n>>>')
+            dataObj = wantDowSc.recv(1024)  #[filename, number, segments]
+            #远程主机强迫关闭了一个现有的连接
+            if not dataObj:
+                continue
+            dataList = eval(dataObj.decode())
+            filename = dataList[0]
+            number = dataList[1]
+            segments = dataList[2]
+            print("Peer", wantDowAddrPort, "want", filename, number, '|', segments)
+            filePath = os.path.join(os.path.abspath('.'), 'share', filename)
+            fileSize = os.path.getsize(filePath)
+            stChunk = fileSize/segments
+            start = int(stChunk*number)
+            readSize = int(stChunk+fileSize%stChunk if number==segments-1 else stChunk)
+
+            preSend = str(readSize)
+            wantDowSc.send(preSend.encode()) #先送要传输的文件大小
+
+            sentSize = 0
+            with open(filePath, 'rb') as f:
+                f.seek(start, 0)
+                while sentSize < readSize:
+                    remainSize = readSize - sentSize
+                    sendSize = 1024 if remainSize > 1024 else remainSize
+                    sendFile = f.read(sendSize)
+                    sentSize += sendSize
+                    wantDowSc.send(sendFile)
+            wantDowSc.close()
+            print("completely send", filename, number, '|', segments)
+
+def main():
+    peer = P2Pclient((HOST, PORT))
+    while peer.register() == False:
+        print("Regiser fell, trying again now...")
+
+    a_thread = SendFileThread() #创建供其他peer下载的地址接口，并传输文件
+    a_thread.setDaemon(True)
+    print("\n" + str(PEERADDRPORT) + "is open for other peers to download")
+    a_thread.start()
+    print("getl: get file list\ndowf: download file\nquit: quit and log out")
+    while True:
+        command = input(">>>")
+        if command.lower() == 'getl':
+            while peer.getl() == False:
+                print("get file list fell, trying again now...")
+        elif command.lower() == 'dowf':
+            while peer.dowf() == False:
+                print("down file fell, trying again now...")
+        elif command.lower() == 'quit': #最好正常退出
+            while peer.logOut() == False:
+                print("log out fell, trying again now...")
+            break
+
+
+if __name__ == '__main__':
+    main()
---
+++ P2Pserver.py
@@ -0,0 +1,86 @@
+#encoding:utf-8
+
+import socketserver
+
+fileDir = {}    #临界资源，是否要加锁？待升级强制关闭连接需要清理fileDir，强制退出待清空字典！！！
+
+class PeerMsg():
+    def __init__(self, addrPort, peerObj):
+        self.addrPort = addrPort
+        self.peerObj = peerObj
+
+class P2Pserver(socketserver.BaseRequestHandler):
+    def handle(self):
+        """main while for server to handle each connection"""
+        print("Get connection from", self.client_address)
+        while(True):
+            peerObj = self.request
+            data = peerObj.recv(1024)
+            if not data:
+                continue
+            if data.decode() == 'register':
+                print(self.client_address, "want to register...")
+                peerObj.send('give files you have'.encode())
+                filenamesObj = peerObj.recv(1024)    #空列表是否为空？
+                if not filenamesObj:
+                    continue
+                filenames = eval(filenamesObj.decode())
+                peerObj.send('give your dowAddrPort'.encode())
+                dowAddrPortObj = peerObj.recv(1024)
+                if not dowAddrPortObj:
+                    continue    #是否应该continue?不管了
+                dowAddrPort = eval(dowAddrPortObj.decode())
+                print("get the register peer download address and port", dowAddrPort)
+                for a_filename in filenames:
+                    a_PeerMsg = PeerMsg(dowAddrPort, peerObj)
+                    if a_filename not in fileDir:  #在fileList中是否存在这个文件名？
+                        fileDir[a_filename] = [a_PeerMsg]
+                    else:
+                        fileDir[a_filename].append(a_PeerMsg)
+                print(self.client_address, "register success...")
+                continue
+            elif data.decode() == 'get file list':
+                print(self.client_address, "want to get file list")
+                fileList = list(fileDir.keys())
+                fileList.sort()
+                peerObj.send(repr(fileList).encode())
+                print("file list has sent to", self.client_address)
+                continue
+            elif data.decode() == 'download file':
+                print(self.client_address, "want to download file")
+                peerObj.send('filename'.encode())
+                wantFilenameObj = peerObj.recv(1024)
+                if not wantFilenameObj:
+                    continue
+                wantFilename = wantFilenameObj.decode()
+                print(self.client_address,"want", wantFilename)
+                if wantFilename in fileDir:
+                    peerObj.send('exist and ready'.encode())
+                    wantPeerAddrPortList = [n.addrPort for n in fileDir[wantFilename]]  #may have error
+                    peerObj.send(repr(wantPeerAddrPortList).encode())
+                    print(wantFilename, "peers list have sent...")
+                    continue
+                else:
+                    peerObj.send('not exist'.encode())
+                    print(wantFilename, "not exit")
+                    continue
+            elif data.decode() == 'quit':
+                print(self.client_address, "want to quit")
+                delKeys = []
+                for key,value in fileDir.items():   #删除表中退出peer的信息
+                    for c in value:
+                        if c.peerObj == peerObj:
+                            fileDir[key].remove(c)  #前提是列表中只有一个此peer信息
+                    if not value:   #删除文件名的peer列表为空状况
+                        delKeys.append(key)
+                for delKey in delKeys:
+                    del fileDir[delKey]
+                peerObj.send('log out success'.encode())
+                print(self.client_address, "has logged out")
+                break
+
+
+if __name__ == '__main__':
+    server = socketserver.ThreadingTCPServer(('127.0.0.1',10010), P2Pserver)
+    print('P2P sever is running, waiting for connection...')
+    server.serve_forever()
